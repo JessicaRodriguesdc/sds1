6e01a6a1804f6ef70f40267d569f690e3881f883
JessicaRodriguesdc <jessicarodridc@gmail.com>
2020-09-17 22:57:05 -0300

    POST records

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/GenreRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/GenreRepository.java
@@ -3,9 +3,10 @@ package com.devsuperior.dspesquisa.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.devsuperior.dspesquisa.entities.Genre;
 import com.devsuperior.dspesquisa.entities.Record;
 
 @Repository
-public interface GenreRepository extends JpaRepository<Record, Long>{
+public interface GenreRepository extends JpaRepository<Genre, Long>{
 
 }
